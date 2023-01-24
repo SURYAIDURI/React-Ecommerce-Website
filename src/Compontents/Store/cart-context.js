d488e2d17075bfb01f7f35688ad551dc588e7eaf
SURYA IDURI <tejs4100@gmail.com>
2023-01-24 12:15:55 +0530

    Implementing Login [DIY]

---
+++ src/Compontents/Store/cart-context.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const CartContext= React.createContext({
+    items: [],
+    totalAmount: 0,
+    addItem: (item) => {},
+    removeItem:(id)=>{},
+    emptyCart: () => {},
+    initilizeCart: () => {},
+    mapID: (id) => {},
+})
+
+export default CartContext;
